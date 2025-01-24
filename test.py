34333a2be5a85693ade6adc10e57259713fda6c0
Ankur586 <ankur.singh586@gmail.com>
2025-01-24 17:48:21 +0530

    master data under process

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from utils import get_masterData
+
 excel_file_path = r'D:\0-convert_price\MRP New Update (08-01-25).xlsx'
 df = pd.read_excel(excel_file_path, engine='openpyxl',  index_col=None, skiprows = lambda x: x in [0, 1])
 
@@ -9,10 +11,3 @@ df = df.dropna(how='all', inplace=False, subset=['MRP/-'])
 # Remove rows where all columns are Unnamed:+
 df = df.drop('Unnamed: 6', axis=1)
 
-data = {}
-data.update({
-        'serial_number': df['S.No'].to_list(),
-        'product_names': df['Brand Name'].tolist(),
-        'product_mrp': df['MRP/-'].to_list()
-    })
-print(data)
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import os
 
 def gst_amount(amt: int, gst_rate: float):
     '''
@@ -12,7 +11,47 @@ def gst_amount(amt: int, gst_rate: float):
     return round(pre_gst_amt, 2), round(gst_amt, 2)
 
 
-product_group_1 = ['Bail Kolhu Mustard Oil', 'Bail Kolhu Mustard Oil', 'Bail Kolhu Mustard Oil', 'Bail Kolhu Mustard Oil', 'Bail Kolhu Mustard Oil', 'Bail Kolhu Mustard Oil', 'Bail Kolhu Mustard Oil', 'Nourish Refined Sunflower Oil', 'Nourish Refined Sunflower Oil', 'Nourish Refined Sunflower Oil', 'Nourish Refined Sunflower Oil', 'Nourish Refined Sunflower Oil', 'Nourish Refined Sunflower Oil', 'Nourish Refined Groundnut Oil', 'Nourish Refined Groundnut Oil', 'Nourish Arhar Dal', 'Nourish Arhar Dal', 'Nourish Chana Dal', 'Nourish Chana Dal', 'Nourish Urad Dhuli Dal', 'Nourish Urad Dhuli Dal', 'Nourish Urad Chilka Dal', 'Nourish Urad Chilka Dal', 'Nourish Urad Sabut', 'Nourish Urad Sabut', 'Nourish Moong Dhuli Dal', 'Nourish Moong Dhuli Dal', 'Nourish Moong Chilka Dal', 'Nourish Moong Chilka Dal', 'Nourish Moong Sabut', 'Nourish Moong Sabut', 'Nourish Desi Masoor', 'Nourish Desi Masoor', 'Nourish Masoor Malka', 'Nourish Masoor Malka', 'Nourish Malka Sabut', 'Nourish Malka Sabut', 'Nourish Rajma Chitra', 'Nourish Rajma Chitra', 'Nourish Rajma Lal', 'Nourish Rajma Lal ', 'Nourish Desi Chana', 'Nourish Desi Chana', 'Nourish Kabuli Chana', 'Nourish Kabuli Chana', 'Nourish Maharani Mixed Dal', 'Nourish Roasted Dalia', 'Nourish Roasted Dalia', 'Nourish Roasted Dalia', 'Nourish Lobia', 'Nourish Flaxseed', 'Nourish Chana Besan', 'Nourish Chana Besan', 'Nourish Chana Besan', 'Nourish Maida ', 'Nourish Maida ', 'Nourish Sooji', 'Nourish Chakki Fresh Atta (Red)', 'Nourish Chakki Fresh Atta (Red)', 'Nourish Multigrain Atta', 'Nourish Multigrain Atta', 'Nourish Basmati Rice', 'Nourish Classic Rice', 'Nourish  Royal Rice', 'Nourish  Classic Rice', 'Nourish  Royal Rice', 'Nourish Platinum Rice', 'Nourish Silver Rice', 'Nourish Kalonji', 'Nourish Moti Elaichi', 'Nourish Hari Elaichi', 'Nourish Tej Patta', 'Nourish Jaivtri', 'Nourish Kali Mirch', 'Nourish Ajwain', 
-                   'Nourish Pink Rock Salt (Sendha Namak)', 'Nourish Pink Rock Salt (Sendha Namak)', 'Nourish Pink Rock Salt (Sendha Namak)', 'Nourish Phool Makhana', 'Nourish Jeera', 'Nourish Barik Saunf', 'Nourish Moti Saunf', 'Nourish Methi Dana', 'Nourish Sabut Dhania', 'Nourish Black Salt (Kala Namak)', 'Nourish Black Salt (Kala Namak)', 'Nourish Rai (Mustard Seed)', 'Nourish Kasoori Methi', 'Nourish Laung Sabut', 'Nourish Dal Chini', 'Nourish Desi Ghee', 'Nourish Desi Ghee', 'Nourish Desi Ghee', 'Nourish Macaroni', 'Nourish Macaroni', 'Nourish Macaroni', 'Nourish Vermicelli', 'Nourish Vermicelli', 'Nourish Vermicelli', 'Nourish Roasted Vermicelli', 'Nourish Pasta', 'Nourish Pasta', 'Nourish Pasta', 'Nourish Poha', 'Nourish Tea', 'Nourish Cashew', 'Nourish Walnut', 'Nourish Raisins', 'Nourish Almond', 'Nourish Pistachio', 'Nourish Chia Seeds', 'Nourish Sunflower Seeds ', 'Nourish Pumpkin Seeds ', 'Nourish Gond Katira '],
+productGroup1_GST_5percent = [
+    ('Bail Kolhu Mustard Oil', '15 Ltr '), ('Bail Kolhu Mustard Oil', '5 L '), ('Bail Kolhu Mustard Oil', '2 L '), ('Bail Kolhu Mustard Oil', '1 L '), 
+    ('Bail Kolhu Mustard Oil', '1 L '), ('Bail Kolhu Mustard Oil', '500ml '), ('Bail Kolhu Mustard Oil', '200ml '), ('Nourish Refined Sunflower Oil', '15 Ltr '), 
+    ('Nourish Refined Sunflower Oil', '5 L '), ('Nourish Refined Sunflower Oil', '2 L '), ('Nourish Refined Sunflower Oil', '1 L '), ('Nourish Refined Sunflower Oil', '1 L '),
+    ('Nourish Refined Sunflower Oil', '500ml '), ('Nourish Refined Groundnut Oil', '1 L'), ('Nourish Refined Groundnut Oil', '500ml '), ('Nourish Arhar Dal', '1 Kg.'),
+    ('Nourish Arhar Dal', '500 gm'), ('Nourish Chana Dal', '1 Kg.'), ('Nourish Chana Dal', '500 gm'), ('Nourish Urad Dhuli Dal', '1 Kg.'), ('Nourish Urad Dhuli Dal', '500 gm'), 
+    ('Nourish Urad Chilka Dal', '1 Kg.'), ('Nourish Urad Chilka Dal', '500 gm'), ('Nourish Urad Sabut', '1 Kg.'), ('Nourish Urad Sabut', '500 gm'), 
+    ('Nourish Moong Dhuli Dal', '1 Kg.'), ('Nourish Moong Dhuli Dal', '500 gm'), ('Nourish Moong Chilka Dal', '1 Kg.'), ('Nourish Moong Chilka Dal', '500 gm'), 
+    ('Nourish Moong Sabut', '1 Kg.'), ('Nourish Moong Sabut', '500 gm'), ('Nourish Desi Masoor', '1 Kg.'), ('Nourish Desi Masoor', '500 gm'), ('Nourish Masoor Malka', '1 Kg.'), 
+    ('Nourish Masoor Malka', '500 gm'), ('Nourish Malka Sabut', '1 Kg.'), ('Nourish Malka Sabut', '500 gm'), ('Nourish Rajma Chitra', '1 Kg.'), ('Nourish Rajma Chitra', '500 gm'), 
+    ('Nourish Rajma Lal', '1 Kg.'), ('Nourish Rajma Lal ', '500 gm'), ('Nourish Desi Chana', '1 Kg.'), ('Nourish Desi Chana', '500 gm'), ('Nourish Kabuli Chana', '1 Kg.'), 
+    ('Nourish Kabuli Chana', '500 gm'), ('Nourish Maharani Mixed Dal', '500 gm'), ('Nourish Roasted Dalia', '1 Kg.'), ('Nourish Roasted Dalia', '500 gm'), 
+    ('Nourish Roasted Dalia', '200 gm'), ('Nourish Lobia', '500 gm'), ('Nourish Flaxseed', '200 gm'), ('Nourish Chana Besan', '1 Kg.'), ('Nourish Chana Besan', '500 gm'),
+    ('Nourish Chana Besan', '200 gm'), ('Nourish Maida ', '1 Kg '), ('Nourish Maida ', '500 gm'), ('Nourish Sooji', '500 gm'), ('Nourish Chakki Fresh Atta (Red)', '10 Kg'),
+    ('Nourish Chakki Fresh Atta (Red)', '5 Kg'), ('Nourish Multigrain Atta', '1 Kg.'), ('Nourish Multigrain Atta', '5 Kg'), ('Nourish Basmati Rice', '1 Kg.'),
+    ('Nourish Classic Rice', '1 Kg.'), ('Nourish  Royal Rice', '1 Kg.'), ('Nourish  Classic Rice', '5 Kg'), ('Nourish  Royal Rice', '5 Kg'), ('Nourish Platinum Rice', '5 Kg'),
+    ('Nourish Silver Rice', '5 Kg'), ('Nourish Kalonji', '100 gm'), ('Nourish Moti Elaichi', '50 gm'), ('Nourish Hari Elaichi', '50 gm'), ('Nourish Tej Patta', '50 gm'),
+    ('Nourish Jaivtri', '25 gm'), ('Nourish Kali Mirch', '100 gm'), ('Nourish Ajwain', '100 gm'), ('Nourish Phool Makhana', '50 gm'), ('Nourish Jeera', '100 gm'),
+    ('Nourish Barik Saunf', '100 gm'), ('Nourish Moti Saunf', '100 gm'), ('Nourish Methi Dana', '100 gm'), ('Nourish Sabut Dhania', '100 gm'),
+    ('Nourish Rai (Mustard Seed)', '100 gm'), ('Nourish Kasoori Methi', '25 gm'), ('Nourish Laung Sabut', '50 gm'), ('Nourish Dal Chini', '50 gm'),
+    ('Nourish Vermicelli', '200 gm'), ('Nourish Vermicelli', '450 gm'), ('Nourish Vermicelli', '100 gm'), ('Nourish Roasted Vermicelli', '400 gm'),
+    ('Nourish Poha', '500 gm'), ('Nourish Tea', '250 gm'), ('Nourish Cashew', '250 gm'), ('Nourish Walnut', '250 gm'), ('Nourish Raisins', '250 gm'),
+    ('Nourish Chia Seeds', '250 gm'), ('Nourish Sunflower Seeds ', '250 gm'), ('Nourish Gond Katira ', '50 gm')
+    ]
+                   
+productGroup2_GST_0percent = [
+    ('Nourish Pink Rock Salt (Sendha Namak)', '200 gm'), ('Nourish Pink Rock Salt (Sendha Namak)', '500 gm'), ('Nourish Pink Rock Salt (Sendha Namak)', '1 Kg'), 
+    ('Nourish Black Salt (Kala Namak)', '200 gm'), ('Nourish Black Salt (Kala Namak)', '500 gm')
+    ]
 
-product_group_2 = 'Nourish Pink Rock Salt (Sendha Namak)', 'Nourish Pink Rock Salt (Sendha Namak)', 'Nourish Pink Rock Salt (Sendha Namak)'
+productGroup3_GST_12percent = [
+    ('Nourish Desi Ghee', '1 L'), ('Nourish Desi Ghee', '500 ml'), ('Nourish Desi Ghee', '200 ml'),('Nourish Macaroni', '200 gm'), ('Nourish Macaroni', '450 gm'),
+    ('Nourish Macaroni', '85 gm'),('Nourish Pasta', '200 gm'), ('Nourish Pasta', '450 gm'), ('Nourish Pasta', '85 gm'),('Nourish Almond', '250 gm'), 
+    ('Nourish Pistachio', '250 gm'), ('Nourish Pumpkin Seeds ', '250 gm'),
+    ]
+
+def get_masterData():
+    data = {}
+    data.update({
+            'GST 5%': productGroup1_GST_5percent,
+            'GST 0%': productGroup2_GST_0percent,
+            'GST 12%': productGroup3_GST_12percent
+        })
+    return data
